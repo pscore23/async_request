7cc6cd68d85e0ecc842cc8b61d6cfa86716f28af
pscore23 <rubik2357x@gmail.com>
2022-09-20 18:07:14 +0900

    更新

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import asyncio
+import json
 from typing import Any, Coroutine
 
 import aiohttp
 from aiohttp import ClientSession
 
+import _exceptions
+
 
 class _BaseAsyncRequester:
-    def __init__(self, url: str, repeat: int) -> None:
+    def __init__(self, url: str, repeat: int, method: str, data: Any) -> None:
         self.url: str = url
         self.repeat: int = repeat
+        self.method: str = method
+        self.data: Any = data
     
     async def _get_data(self, url: str, __session: ClientSession) -> Coroutine[Any, Any, Any]:
         async with __session.get(url) as response:
+            return await response.json()
+    
+    async def _post_data(self, url: str, data: Any, __session: ClientSession) -> Coroutine[Any, Any, Any]:
+        async with __session.post(url, data=json.dumps(data)) as response:
             return await response.json()
     
     async def _base_request(self) -> Any:
@@ -19,7 +28,14 @@ class _BaseAsyncRequester:
             tasks: list[Any] = []
             
             for i in range(self.repeat):
-                tasks.append(asyncio.ensure_future(self._get_data(self.url, session)))
+                if self.method == "GET":
+                    tasks.append(asyncio.ensure_future(self._get_data(self.url, session)))
+                
+                elif self.method == "POST":
+                    tasks.append(asyncio.ensure_future(self._post_data(self.url, self.data, session)))
+                
+                else:
+                    raise _exceptions.InputError("指定されたメソッドが見つかりません")
             
             original_data: Any = await asyncio.gather(*tasks)
             
@@ -27,8 +43,8 @@ class _BaseAsyncRequester:
 
 
 class AsyncRequester(_BaseAsyncRequester):
-    def __init__(self, url: str, repeat: int) -> None:
-        super().__init__(url, repeat)
+    def __init__(self, url: str, repeat: int, method: str = "GET", data: Any = {}) -> None:
+        super().__init__(url, repeat, method, data)
     
     def request(self) -> Any:
         return asyncio.run(self._base_request())
